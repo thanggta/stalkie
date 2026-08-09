4a4d77f9a108188b2e7c81c285112a7bc340f0fb
Theodore <theodoredanton15@gmail.com>
2026-08-09 12:27:01 +0700

    feat: case validator enforcing INV-2, INV-3, INV-4

--- lib/carve_core.dart
+++ lib/carve_core.dart
@@ -3,3 +3,4 @@ export 'src/models/fragment.dart';
 export 'src/models/case_file.dart';
 export 'src/rules/predicate.dart';
 export 'src/loader/case_parser.dart';
+export 'src/loader/validator.dart';
